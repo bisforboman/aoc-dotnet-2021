6fba437fdbbb60a68955481603c699c19f5a4c86
Jakob Boman <jakbo@vinngroup.net>
2022-10-30 13:42:51 +0100

    Add FSharp solution to day 1

---
+++ 1/FSharp/part1.fsx
@@ -0,0 +1,22 @@
+let readLines filePath = System.IO.File.ReadAllLines(filePath); 
+
+let parsedInput = 
+    Seq.ofArray (readLines "../largeinput.txt")
+    |> Seq.map System.Int32.Parse
+    |> Seq.toList
+
+let increasedMeasurements m = 
+    Seq.zip (0 :: m) m
+    |> Seq.skip 1
+    |> Seq.countBy (fun (x, y) -> x < y)
+    |> Seq.toList
+
+printfn "Part1 results: %A" (increasedMeasurements parsedInput)
+
+let slidingWindow =
+    Seq.zip3 (0 :: 0 :: parsedInput) (0 :: parsedInput) parsedInput
+    |> Seq.skip 2
+    |> Seq.map (fun (x, y, z) -> x + y + z)
+    |> Seq.toList
+
+printfn "Part2 results: %A" (increasedMeasurements slidingWindow)

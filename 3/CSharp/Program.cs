cf4e2b978ce2b219d14ba63fd913c91febf6ba6b
Jakob Boman <jakbo@vinngroup.net>
2022-11-01 22:42:53 +0100

    Rework previous solution to make it simpler for part 2

--- 3/CSharp/Program.cs
+++ 3/CSharp/Program.cs
@@ -10,28 +10,26 @@ public static class Program
     private static IEnumerable<int[]> ToEnumerable(this string[] s) => 
         s.Select(i => i.ToCharArray().Select(i => i - '0').ToArray());
 
+    private record Bits(int Zeros, int Ones);
+
+    private static Bits MostCommonValueAt(this IEnumerable<int[]> lists, int i)
+    {
+        var ones = lists.Sum(l => l[i]);
+        return new(lists.Count() - ones, ones);
+    }
+
     private static void Part1(string inputPath)
     {
         var input = ReadAndParseInput(inputPath);
 
-        var result = new int[input[0].Length];
-
-        foreach (var list in input)
-        {
-            for (int i = 0; i < list.Length; i++)
-            {
-                result[i] += list[i];
-            }
-        }
-
         var gamma = 0;
         var epsilon = 0;
-        for (int i = 0; i < result.Length; i++)
+        for (int i = 0; i < input[0].Length; i++)
         {
             gamma <<= 1;
             epsilon <<= 1;
 
-            if (result[i] > input.Length/2)
+            if (input.MostCommonValueAt(i).HasMoreOnes())
             {
                 gamma += 1;
             }
@@ -44,14 +42,45 @@ public static class Program
         Console.WriteLine("Part1 result is: " + gamma * epsilon);
     }
 
+    private static int FromIntArray(this int[] array)
+    {
+        var ret = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            ret <<= 1;
+            ret += array[i];
+        }
+        return ret;
+    }
+
+    private static bool HasMoreOnes(this Bits b) => b.Ones >= b.Zeros;
+
+    private static int[] Resolve(IEnumerable<int[]> input, int ifTrue, int ifFalse)
+    {
+        int i = 0;
+        var listToReduce = new List<int[]>(input);
+        while (listToReduce.Count > 1)
+        {
+            var pivot = listToReduce.MostCommonValueAt(i).HasMoreOnes() 
+                ? ifTrue
+                : ifFalse;
+
+            listToReduce.RemoveAll(item => item[i] != pivot);
+
+            i++;
+        }
+
+        return listToReduce.First();
+    }
+
     private static void Part2(string inputPath)
     {
         var input = ReadAndParseInput(inputPath);
 
-        var oxygenGeneratorRating = 0;
-        var cO2ScrubberRating = 0;
-
-        // Console.WriteLine("Part2 result: " + result);
+        var oxygen = Resolve(input, 1, 0).FromIntArray();
+        var cO2Scrubber = Resolve(input, 0, 1).FromIntArray();
+        
+        Console.WriteLine("Part2 result: " + oxygen * cO2Scrubber);
     }
 
     public static void Main(string[] args)

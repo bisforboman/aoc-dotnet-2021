874c4b679be82f451bdda24cc205be6b0357a85a
Jakob Boman <jakbo@vinngroup.net>
2022-11-16 21:12:07 +0100

    Add additional folders

---
+++ 8/CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp;
+
+public static class Program
+{
+    private static string[] ReadAndParseInput(string filePath) => 
+        File.ReadAllLines(filePath).ToArray();
+
+    private static void Part1(string inputPath)
+    {
+        Console.WriteLine("Part1 result is: ");
+    }
+
+    private static void Part2(string inputPath)
+    {
+        Console.WriteLine("Part2 result is: ");
+    }
+
+    public static void Main(string[] args)
+    {
+        Part1(args[0]);
+        Part2(args[0]);
+    }
+}

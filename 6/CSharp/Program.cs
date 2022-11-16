84d44b5e721e6f6e96b37f8c5d87008754363d7d
Jakob Boman <jakbo@vinngroup.net>
2022-11-16 23:03:16 +0100

    Add solutions for day 6

--- 6/CSharp/Program.cs
+++ 6/CSharp/Program.cs
@@ -1,18 +1,70 @@
-﻿namespace CSharp;
+﻿using System.Diagnostics;
+
+namespace CSharp;
 
 public static class Program
 {
-    private static string[] ReadAndParseInput(string filePath) => 
-        File.ReadAllLines(filePath).ToArray();
+    private static int[] ReadAndParseInput(string filePath) => 
+        File.ReadAllText(filePath)
+            .Split(',')
+            .Select(int.Parse)
+            .ToArray();
 
     private static void Part1(string inputPath)
     {
-        Console.WriteLine("Part1 result is: ");
+        var fishes = ReadAndParseInput(inputPath)
+            .ToList();
+
+        int newFishesCount;
+        for (int i = 0; i < 80; i++)
+        {
+            newFishesCount = 0; 
+            for (int j = 0; j < fishes.Count; j++)
+            {
+                if (fishes[j] == 0)
+                {
+                    newFishesCount++;
+                    fishes[j] = 6;
+                }
+                else
+                {
+                    fishes[j]--;
+                }
+            }
+
+            fishes.AddRange(Enumerable.Repeat(8, newFishesCount));
+        }
+
+        Console.WriteLine("Part1 result is: " + fishes.Count);
     }
 
     private static void Part2(string inputPath)
     {
-        Console.WriteLine("Part2 result is: ");
+        var fishes = ReadAndParseInput(inputPath)
+            .GroupBy(i => i)
+            .ToDictionary(i => i.Key, i => i.Count());
+
+        var arr = new long[9];
+        foreach (var f in fishes)
+        {
+            arr[f.Key] = f.Value;
+        }
+
+        long newFishesCount;
+        for (int i = 0; i < 256; i++)
+        {
+            newFishesCount = arr[0];
+
+            for (int j = 1; j < arr.Length; j++)
+            {
+                arr[j-1] = arr[j];
+            }
+
+            arr[8] = newFishesCount;
+            arr[6] += newFishesCount;
+        }
+
+        Console.WriteLine("Part2 result is: " + arr.Sum());
     }
 
     public static void Main(string[] args)
